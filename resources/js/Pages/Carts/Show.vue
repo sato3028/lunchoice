34d2b8c75060d8d7145da9b09b2c8199e9fce49f
sato3028 <sakayusho007@gmail.com>
2024-01-21 02:42:56 +0900

    購入ページのスタイルの変更

--- resources/js/Pages/Carts/Show.vue
+++ resources/js/Pages/Carts/Show.vue
@@ -8,59 +8,105 @@ const submitOrder = () => {
 </script>
 
 <template>
-    <div>
-      <Header_Component />
-  
-      <h1>購入確認</h1>
-  
-      <div class="credit-card-info">
-        <h2>クレジットカード情報</h2>
-        <div class="form-group">
-          <label for="card-number">カード番号</label>
-          <input type="text" id="card-number" disabled placeholder="1234 5678 9012 3456" />
+  <Header_Component />
+    <div class="min-w-screen min-h-screen flex justify-center px-5">
+    <div class="w-full mx-auto rounded-lg bg-white p-5 text-gray-700" style="max-width: 600px">
+        <div class="w-full pt-1 pb-5">
         </div>
-        <div class="form-group">
-          <label for="expiry-date">有効期限</label>
-          <input type="text" id="expiry-date" disabled placeholder="MM/YY" />
+        <div class="mb-10">
+            <h1 class="text-center font-bold text-xl uppercase">支払い</h1>
         </div>
-        <div class="form-group">
-          <label for="cvc">CVC</label>
-          <input type="text" id="cvc" disabled placeholder="123" />
+        <div class="mb-3 flex -mx-2">
+            <div class="px-2">
+                <label for="type1" class="flex items-center cursor-pointer">
+                    <input type="radio" class="form-radio h-5 w-5 text-blue" name="type" id="type1" checked>
+                    <img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png" class="h-8 ml-3">
+                </label>
+            </div>
+            <div class="px-2">
+                <label for="type2" class="flex items-center cursor-pointer">
+                    <input type="radio" class="form-radio h-5 w-5 text-blue" name="type" id="type2">
+                    <img src="https://www.sketchappsources.com/resources/source-image/PayPalCard.png" class="h-8 ml-3">
+                </label>
+            </div>
         </div>
-      </div>
-      <div class="page_control_button_buck">
-        <Link href="/carts" class="go-back-button">カートに戻る</Link>
-      </div>
-      <button @click="submitOrder">購入</button>
+        <div class="mb-3">
+            <label class="font-bold text-sm mb-2 ml-1">名前</label>
+            <div>
+                <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text"/>
+            </div>
+        </div>
+        <div class="mb-3">
+            <label class="font-bold text-sm mb-2 ml-1">カード番号</label>
+            <div>
+                <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text"/>
+            </div>
+        </div>
+        <div class="mb-3 -mx-2 flex items-end">
+            <div class="px-2 w-1/2">
+                <label class="font-bold text-sm mb-2 ml-1">有効期限</label>
+                <div>
+                    <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                        <option value="01">01 - January</option>
+                        <option value="02">02 - February</option>
+                        <option value="03">03 - March</option>
+                        <option value="04">04 - April</option>
+                        <option value="05">05 - May</option>
+                        <option value="06">06 - June</option>
+                        <option value="07">07 - July</option>
+                        <option value="08">08 - August</option>
+                        <option value="09">09 - September</option>
+                        <option value="10">10 - October</option>
+                        <option value="11">11 - November</option>
+                        <option value="12">12 - December</option>
+                    </select>
+                </div>
+            </div>
+            <div class="px-2 w-1/2">
+                <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                    <option value="2027">2027</option>
+                    <option value="2028">2028</option>
+                    <option value="2029">2029</option>
+                    <option value="2030">2030</option>
+                </select>
+            </div>
+        </div>
+        <div class="mb-10">
+            <label class="font-bold text-sm mb-2 ml-1">CVC番号</label>
+            <div>
+                <input class="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="000" type="text"/>
+            </div>
+        </div>
+        <div>
+            <button @click="submitOrder" class="block w-full max-w-xs mx-auto text-white bg-blue-btn rounded-lg px-3 py-3 font-semibold"><i class="mdi mdi-lock-outline mr-1"></i> 購入</button>
+        </div>
+        <div class="mt-4">
+          <Link
+            href="/carts"
+            class="block w-full max-w-xs mx-auto bg-gray-500 hover:bg-gray-700 focus:bg-gray-700 text-white rounded-lg px-3 py-3 font-semibold text-center">キッチンカーリストに戻る</Link>
+        </div>
     </div>
+</div>
   </template>
   
   <style>
-  .credit-card-info {
-    margin-bottom: 20px;
+  .bg-blue-btn{
+    background-color: #29A8E1;
   }
   
-  .form-group {
-    margin-bottom: 10px;
-  }
-  
-  .form-group label {
-    display: block;
-  }
-  
-  .form-group input {
-    width: 100%;
-    padding: 8px;
+  .bg-blue-btn:hover{
+    background-color: #2387C1;
   }
 
-  .page_control_button_buck a {
-  text-align: center;
-  display:block;
-  padding:14px 20px;
-  width:100%;
-  background-color: #727171;
-  color:#fff;
-  border-radius:14px;
-}
+  .text-blue{
+    color: #29A8E1;
+  }
+
+  .text-blue:hover{
+    color: #2387C1;
+  }
   </style>
   
